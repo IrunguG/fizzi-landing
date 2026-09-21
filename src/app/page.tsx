202385da318c5d86f6301e4fe69c6ef58431eb2f
irungug <irungug@gmail.com>
2026-09-21 17:00:00 +0300

    Footer animation

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 import { Alternation } from "@/components/alternation/Alternation";
 import { Carousel } from "@/components/carousel/Carousel";
+import { Footer } from "@/components/Footer";
 import { Header } from "@/components/Header";
 import { Hero } from "@/components/Hero/Hero";
 import { Skydive } from "@/components/skydive/Skydive";
@@ -12,6 +13,7 @@ export default function Home() {
 		<Skydive />
 		<Carousel />
 		<Alternation />
+		<Footer />
 	</main>
   );
 }
